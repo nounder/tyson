9003bdcd1021bad21952bb0c172af5cd436d6d60
Rafael Gutkowski <nounderline@libred.org>
2024-08-17 15:10:44 +0200

    More defined decode typing

--- decode.ts
+++ decode.ts
@@ -2,11 +2,21 @@ import Types from "./types.ts";
 import { isPlainObject } from "./utils.ts";
 
 type JsonPrimitive = null | boolean | number | string;
-type JsonValue = JsonPrimitive | JsonPrimitive[] | JsonObject;
+type JsonValue = JsonPrimitive | JsonValue[] | JsonObject;
 type JsonObject = { [key: string]: JsonValue };
 
 type RefObject = { $ref: string };
 type TypedObject = { $type: string };
+
+// JS types without functions and symbols
+type DecodableJsValue =
+  | undefined
+  | boolean
+  | number
+  | bigint
+  | string
+  | null
+  | { [key: string]: DecodableJsValue };
 
 type EncodedJsonObject =
   | JsonObject
@@ -25,7 +35,9 @@ type EncodedDocument =
   | JsonObject
   | (JsonObject & { $defs: EncodedDefinitionMap });
 
-export function decodeValue(value: EncodedJsonObject) {
+export function decodeValue(
+  value: JsonValue | EncodedJsonObject,
+): any {
   if (
     typeof value === "object" &&
     value !== null &&
@@ -47,10 +59,45 @@ export function decodeValue(value: EncodedJsonObject) {
   return value;
 }
 
+export function decodeObject(target: EncodedDocument): void {
+  // First we have to deref defs
+  if (target.$defs) {
+    derefDefs(target.$defs as EncodedDefinitionMap);
+  }
+
+  for (const key in target) {
+    if (key === "$defs") {
+      continue;
+    }
+
+    const value = target[key];
+
+    if (isPlainObject(value)) {
+      derefObject(value, (target.$defs as EncodedDefinitionMap) || {});
+    }
+  }
+}
+
+function derefDefs(defs: EncodedJsonObject): void {
+  const stuntDefs = Object.fromEntries(
+    Object.keys(defs).map((k) => [k, {}]),
+  );
+
+  // Deref definitions so they point to stunts
+  // we're going to shape later
+  derefObject(defs, stuntDefs);
+
+  // Now that we have all refs deref'd into stunt refs,
+  // let's shape them into actual refs :')
+  Object.entries(stuntDefs).forEach(([k, v]) => {
+    Object.assign(v, defs[k]);
+  });
+}
+
 function derefObject(
   target: EncodedJsonObject,
   defs: EncodedDefinitionMap,
-) {
+): void {
   for (const [k, v] of Object.entries(target)) {
     if (isPlainObject(v)) {
       const $ref = v["$ref"] as string;
@@ -67,17 +114,5 @@ function derefObject(
         derefObject(v, defs);
       }
     }
-  }
-
-  return defs;
-}
-
-export function decodeObject(target: object) {
-  if ("$defs" in target) {
-    const { $defs } = target;
-
-    // TODO: Handle circular refs
-    // TODO: Throw on missing refs
-    // TODO: Throw on missing refs
   }
 }
